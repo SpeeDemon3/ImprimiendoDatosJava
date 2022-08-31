90a2fe7c23092920d38f3445029c782a389c10fc
Antonio Ruiz Benito <109352678+SpeeDemon3@users.noreply.github.com>
2022-08-31 11:40:46 +0200

    Add files via upload

---
+++ ImprimiendoDatos/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Tipos de datos
+         */
+
+        // Numeros Enteros
+        byte number1 = 4;
+        System.out.println(number1);
+
+        short number2 = 8;
+        System.out.println(number2);
+
+        int number3 = 163;
+        System.out.println(number3);
+
+        long number4 = 1987;
+        System.out.println(number4);
+
+        // Numeros Decimales (float)
+        float number5 = 3.3f;
+        System.out.println(number5);
+
+        double number6 = 3.33d;
+        System.out.println(number6);
+
+        //Caracter (char)
+        char character = 'a';
+        System.out.println(character);
+
+        // Cadenas Texto (Strings)
+        String name = "Antonio";
+        System.out.println(name);
+
+        // Tipos envoltorio
+        Integer number7 = null;
+        System.out.println(number7);
+
+        Long number8 = 2L;
+        System.out.println(number8);
+
+
+
+    }
+}
